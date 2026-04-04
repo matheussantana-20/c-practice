cdd8df2865eff428da92d9f382adcc01f1aeb397
matheus <msantananicacio@gmail.com>
2026-04-04 12:19:01 -0300

    sistema de verificacao de idade para votar

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+int idade;
+char nome[20];
+// sistema de votos !
+int main()
+{
 
-int main() {
-    printf("Olá, mundo!\n");
+    // pede o nome do usuario !
+    printf("digite o seu nome:\n");
+    scanf(" %s", nome);
+
+    // pede a idade do usuario!
+    printf("diga a sua idade:");
+    scanf(" %d", &idade);
+
+    // verifica a idade do usuario!
+    if (idade >= 18)
+    {
+        printf("ja possui idade para votar!");
+    }
+    else
+    {
+        printf("ainda nao possui idade para votar");
+    }
+
     return 0;
 }
